2d508e13c2c66de1215bdadd2c0e050242306e4c
Prajith Nair <prajithnair98@gmail.com>
2020-04-08 23:08:10 +0530

    second commit

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -16,16 +16,15 @@ class Background extends Component
         this.data={}
     }
     componentDidMount(){
-        fetch(`https://cdn.abplive.com/coronastats/prod/coronastats.json`)
+
+            fetch(`https://api.covid19india.org/data.json`)
             .then(response => response.json())
             .then((jsonData) => {
-                this.setState(jsonData)
+                this.setState(jsonData.statewise[0])
             })
             .catch((error) => {
-    // handle your errors here
             console.error(error)
             })
-            
     }
     render(){
         return (
@@ -34,9 +33,9 @@ class Background extends Component
                     <h2 className='pandemic'>PANDEMIC</h2>
                     <div className='main'>   
                     <img className='back' src={back} alt="Trulli" ></img>
-                        <Total value={this.state.totalcases}/>
-                        <Death value={this.state.totaldeaths}/>
-                        <Recover value={this.state.totalrecovered}/>  
+                        <Total value={this.state.confirmed}/>
+                        <Death value={this.state.deaths}/>
+                        <Recover value={this.state.recovered}/>  
                     </div>
                     <marquee hspace="320" behavior="scroll"  direction="left">Central Helpline Number for CoronaVirus: <a href="+91-11-23978046">+91-11-23978046</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; Helpline Email ID: <a href="ncov2019@gov.in">ncov2019@gov.in</a> or <a href="ncov2019@gmail.com">ncov2019@gmail.com</a></marquee>
                     <Prevent/>
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,59 +1,58 @@
-import React, { Component } from 'react'
-import './Background.css'
-import data from "./output"; 
+// import React, { Component } from 'react'
+// import './Background.css'
 
-class Table extends Component {
-    constructor(props) {
-        super(props);
-        var stats=[]
-    }
+// class Table extends Component {
+//     constructor(props) {
+//         super(props);
+//         var stats=[]
+//     }
     
-    callAPI() {
-        fetch("http://localhost:9000/scrape")
-        .then(response => response.json())
-        .then((jsonData) => {
-            this.stats=jsonData
-        })
-        .catch((error) => {
-        console.error(error)
-        })
-    }
+//     callAPI() {
+//         fetch("http://localhost:9000/scrape")
+//         .then(response => response.json())
+//         .then((jsonData) => {
+//             this.stats=jsonData
+//         })
+//         .catch((error) => {
+//         console.error(error)
+//         })
+//     }
     
-    componentWillMount() {
-        this.callAPI();
-        console.log("from table : "+this.stats);
-    }
+//     componentWillMount() {
+//         this.callAPI();
+//         console.log("from table : "+this.stats);
+//     }
     
-    render() {
-        return (
-            <div>
-                <table border="1"  align="center" className='stats'>
-                    <thead>
-                        <tr>
-                            <th>S. No.</th>
-                            <th>Name of State / UT</th>
-                            <th>Total Confirmed cases </th>
-                            <th>Cured/Discharged/Migrated</th>	
-                            <th>Death</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            data.map((value,index) => {
-                                return <tr key={index}>
-                                    <td>{value[0]}</td>
-                                    <td>{value[1]}</td>
-                                    <td>{value[2]}</td>
-                                    <td>{value[3]}</td>
-                                    <td>{value[4]}</td>
-                                </tr>
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <table border="1"  align="center" className='stats'>
+//                     <thead>
+//                         <tr>
+//                             <th>S. No.</th>
+//                             <th>Name of State / UT</th>
+//                             <th>Total Confirmed cases </th>
+//                             <th>Cured/Discharged/Migrated</th>	
+//                             <th>Death</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             data.map((value,index) => {
+//                                 return <tr key={index}>
+//                                     <td>{value[0]}</td>
+//                                     <td>{value[1]}</td>
+//                                     <td>{value[2]}</td>
+//                                     <td>{value[3]}</td>
+//                                     <td>{value[4]}</td>
+//                                 </tr>
+//                             })
+//                         }
+//                     </tbody>
+//                 </table>
+//             </div>
+//         )
+//     }
+// }
 
-export default Table
+// export default Table
