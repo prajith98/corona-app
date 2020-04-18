3161cc0028256b338a48114f9ec94ebad0c67d87
Prajith Nair <prajithnair98@gmail.com>
2020-04-18 18:14:59 +0530

    Updates

---
+++ precache-manifest.56cf2e0bd75f4786ebd9a242cf55018a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d24f2b61db883200a5dddc7450ad0f",
+    "url": "/corona-app/index.html"
+  },
+  {
+    "revision": "01f1b7ce96df82d0a3d4",
+    "url": "/corona-app/static/css/main.b53ea589.chunk.css"
+  },
+  {
+    "revision": "eb75784e0db683b23885",
+    "url": "/corona-app/static/js/2.c524d79c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-app/static/js/2.c524d79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f1b7ce96df82d0a3d4",
+    "url": "/corona-app/static/js/main.6c944f24.chunk.js"
+  },
+  {
+    "revision": "a74ea87a626e436384c4",
+    "url": "/corona-app/static/js/runtime-main.785bd798.js"
+  },
+  {
+    "revision": "198d755cd5f2f6dd7dd160e60a169c9f",
+    "url": "/corona-app/static/media/back.198d755c.jpg"
+  }
+]);
